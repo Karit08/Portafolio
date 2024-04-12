6db0b60ca7f1543839c923fac889a8b5940c9c5e
Karit08 <karla.jm.ingmec49@gmail.com>
2024-04-11 19:45:18 -0600

    Se modifico el componente Vocational

--- src/components/Vocational.jsx
+++ src/components/Vocational.jsx
@@ -1,8 +1,50 @@
 
-const Vocational = ()=>{
-    return(
+const Vocational = () => {
+    return (
         <>
-         FORMACIÓN
+            {/* Codind Dojo */}
+            <div>
+                <div>
+                    <img src="https://cutecdn.codingdojo.com/svg_images/logos/coding_dojo_original.svg" alt="Coding Dojo" />
+                    {/* https://the-winston-project.imgix.net/610beec5aea874132acbafda/26_codingdojo-logo.png */}
+                </div>
+                <div>
+                    <h3><strong>Programa AWS RE/START en Amazon Web Services (AWS)</strong><br />
+                        2023-2024</h3>
+                </div>
+            </div>
+            {/* Alura Latam */}
+            <div>
+                <div>
+                    <img src="https://aprende.goodneighbors.cl/wp-content/uploads/2022/02/ONE_logo_rgb-768x408.png" alt="Alura Latam" />
+                    <img src="https://raw.githubusercontent.com/joshuaFrias95/Encriptador-Alura-Oracle-ONE/main/img/readme/aluraoracle.png" alt="alura y Oracle" />
+                </div>
+                <div>
+                    <h3><strong>Back-end</strong> <br /> 2023-2022 </h3>
+                </div>
+            </div>
+            {/* Henry */}
+            <div>
+                <a href="https://certificates.soyhenry.com/new-cert?id=c30d61ff3c71d827d9101672429203829deda7cab0a146b54c8de22e0c0edea5" type="_blank">
+                <div>
+                    <img src="https://assets.soyhenry.com/logos/LOGO-HENRY-04.png" alt="Henry" />
+                </div>
+                </a>
+                <div>
+                    <h3><strong>FullStack Developer</strong><br /> 2022-2023</h3>
+                </div>
+
+            </div>
+            {/* Mectronica */}
+            <div>
+                <div>
+                <img src="https://sgei.mx/UPVT/(S(mx25o3a3ujk3kvo3c1ibqrip))/assets/img/logo.png" alt="UPVT" />
+                </div>
+                <div>
+                    <h3><strong>Ingeniería Mecatrónica</strong><br /> Titulada</h3>
+                </div>
+            </div>
+
         </>
     )
 };
